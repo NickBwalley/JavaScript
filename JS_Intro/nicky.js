6d0726bb153aa79aabf9b8558df4ed62b4502600
NickBwalley <nickbwalley@gmail.com>
2020-07-24 11:47:21 +0300

    JavaScript

--- JS_Intro/nicky.js
+++ JS_Intro/nicky.js
@@ -2,7 +2,3 @@ function sayHelloToNick(){
 	alert("This is my First JavaScript Application!..")
 }
 
-var money;
-money = 90000;
-var name = "Nick Bwalley";
-
---
+++ JS_Intro/operators.js
@@ -0,0 +1,56 @@
+function operators(){
+	var a = 20;
+	var b = 30;
+	var c = "Text";
+	var linebreak = "<br/>";
+
+	document.write("a + b = ");
+	result = a + b;
+	document.write(result);
+	document.write(linebreak);
+
+	document.write("a - b = ");
+	result = a - b;
+	document.write(result);
+	document.write(linebreak);
+
+	document.write("a * b = ");
+	result = a * b;
+	document.write(result);
+	document.write(linebreak);
+
+	document.write("a / b = ");
+	result = a / b;
+	document.write(result);
+	document.write(linebreak);
+
+	document.write("a % b = ");
+	result = b % a;
+	document.write(result);
+	document.write(linebreak);
+
+	document.write("a + b + c = ");
+	result = a + b + c;
+	document.write(result);
+	document.write(linebreak);
+
+	a = a++;
+	document.write("a++ = ");
+	result = a++;
+	document.write(result);
+	document.write(linebreak);
+	document.write(a);
+	document.write(linebreak);
+
+	b = b++;
+	document.write("b++ = ");
+	result = b++;
+	document.write(result);
+	document.write(linebreak);
+	document.write(b);
+	document.write(linebreak);
+
+
+}
+// invoking functions 
+operators();
