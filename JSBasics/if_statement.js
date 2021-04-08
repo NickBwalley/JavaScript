0a5665262499231b30586e95378ca86015ebefad
NickBwalley <nickbwalley@gmail.com>
2021-04-08 17:58:46 +0300

    javascript

---
+++ JSBasics/if_statement.js
@@ -0,0 +1,12 @@
+var a = 50;
+if (a > 20){
+    document.writeln("Value of a is greater than 20")
+}
+
+var b = 50;
+if (b%2==0){
+    document.write("b is an even number");
+}
+else{
+    document.write("b is an odd nubmer");
+}
