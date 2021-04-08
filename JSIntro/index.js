7a216146492067013dd9971978d66819b2a63318
NickBwalley <nickbwalley@gmail.com>
2021-04-08 12:35:50 +0300

    JS Intro

---
+++ JSIntro/index.js
@@ -0,0 +1,12 @@
+
+
+
+/*External JavaScript file
+We can create external JavaScript file and embed it in many html page.
+
+It provides code re usability because single JavaScript file can be used in several html pages.
+
+An external JavaScript file must be saved by .js extension. It is recommended to embed all JavaScript files into a single file. It increases the speed of the webpage.
+
+
+Let's create an external JavaScript file that prints Hello Javatpoint in a alert dialog box.*/ 
