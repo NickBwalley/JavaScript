680e4ca62decf22701e8961530ea8503c9c9dc7f
NickBwalley <nickbwalley@gmail.com>
2021-04-08 18:14:52 +0300

    javascript

--- JSBasics/if_statement.js
+++ JSBasics/if_statement.js
@@ -1,7 +1,9 @@
 var a = 50;
 if (a > 20){
-    document.writeln("Value of a is greater than 20")
+    document.writeln("Value of a is greater than 20");
 }
+
+//document.writeln("\n");
 
 var b = 50;
 if (b%2==0){
@@ -10,3 +12,17 @@ if (b%2==0){
 else{
     document.write("b is an odd number");
 }
+
+var a=20;  
+if(a==10){  
+document.write("a is equal to 10");  
+}  
+else if(a==15){  
+document.write("a is equal to 15");  
+}  
+else if(a==20){  
+document.write("a is equal to 20");  
+}  
+else{  
+document.write("a is not equal to 10, 15 or 20");  
+}  
