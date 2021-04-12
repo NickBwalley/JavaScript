65418e048d1c34cdd70142dbed6288c966f415e1
NickBwalley <nickbwalley@gmail.com>
2021-04-12 12:03:30 +0300

    JS DataTypes

--- DataTypes/data_types.js
+++ DataTypes/data_types.js
@@ -42,3 +42,49 @@ document.write("<br>");
 document.write(typeof(x2));
 document.write("<br>");
 document.write(typeof(x3));
+
+// JavaScript Strings
+// Strings are written with quotes. You can use single or double quotes.
+// Example
+var carName1 = "Volvo XC60";   // Using double quotes
+var carName2 = 'Volvo XC60';   // Using single quotes
+
+// You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
+//Example
+var answer1 = "It's alright";             // Single quote inside double quotes
+var answer2 = "He is called 'Johnny'";    // Single quotes inside double quotes
+var answer3 = 'He is called "Johnny"';    // Double quotes inside single quotes
+document.write("<br>");
+document.write(answer2); // ouputs: He is called 'Johnny'
+document.write("<br>");
+/*
+JavaScript Numbers
+JavaScript has only one type of numbers.
+
+Numbers can be written with, or without decimals: */
+var x1 = 34.00;     // Written with decimals
+var x2 = 34;        // Written without decimals
+
+// Extra large or extra small numbers can be written with scientific (exponential) notation:
+var y = 123e5;      // 12300000
+var z = 123e-5;     // 0.00123
+
+// JavaScript Booleans
+// Booleans can only have two values: true or false.
+
+var x = 5;
+var y = 5;
+var z = 6;
+document.write(x == y)       // Returns true
+document.write("<br>");
+document.write(x == z)       // Returns false
+document.write("<br>");
+
+/*
+JavaScript Arrays
+JavaScript arrays are written with square brackets.
+
+Array items are separated by commas.
+
+The following code declares (creates) an array called cars, containing three items (car names):
+ */
