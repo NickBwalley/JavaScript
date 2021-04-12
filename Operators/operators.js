c06d19acf3374bbaab62611c2acc9803f622e33d
NickBwalley <nickbwalley@gmail.com>
2021-04-12 11:39:56 +0300

    JS Operators

--- Operators/operators.js
+++ Operators/operators.js
@@ -82,3 +82,26 @@ void	            it discards the expression's return value.
 yield	            checks what is returned in a generator by the generator's iterator.
 
 */
+
+/* Operator Precedence
+Operator precedence describes the order in which operations are performed in an arithmetic expression. */
+// Example
+var x = 100 + 50 * 3;
+
+/*
+Is the result of example above the same as 150 * 3, or is it the same as 100 + 150?
+
+Is the addition or the multiplication done first?
+
+As in traditional school mathematics, the multiplication is done first.
+
+Multiplication (*) and division (/) have higher precedence than addition (+) and subtraction (-).
+
+And (as in school mathematics) the precedence can be changed by using parentheses:
+
+ */
+// Example
+var x = (100 + 50) * 3;
+/*When using parentheses, the operations inside the parentheses are computed first.
+
+When many operations have the same precedence (like addition and subtraction), they are computed from left to right: */
