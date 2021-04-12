418b626e64febd103e0fca9a0a3fbb7d4921f594
NickBwalley <nickbwalley@gmail.com>
2021-04-12 12:44:48 +0300

    JS Functions

---
+++ Functions/functions.js
@@ -0,0 +1,53 @@
+// JavaScript Functions
+// A JavaScript function is executed when "something" invokes it (calls it).
+function myFunction(p1, p2) {
+    return p1 * p2;   // The function returns the product of p1 and p2
+  }
+
+document.write(myFunction(5, 5));
+
+/*
+JavaScript Function Syntax
+A JavaScript function is defined with the function keyword, followed by a name, followed by parentheses ().
+
+Function names can contain letters, digits, underscores, and dollar signs (same rules as variables).
+
+The parentheses may include parameter names separated by commas:
+(parameter1, parameter2, ...)
+
+The code to be executed, by the function, is placed inside curly brackets: {}
+
+function name(parameter1, parameter2, parameter3) {
+  // code to be executed
+}
+Function parameters are listed inside the parentheses () in the function definition.
+
+Function arguments are the values received by the function when it is invoked.
+
+Inside the function, the arguments (the parameters) behave as local variables.
+
+A Function is much the same as a Procedure or a Subroutine, in other programming languages. */
+
+/*
+Function Invocation
+The code inside the function will execute when "something" invokes (calls) the function:
+
+When an event occurs (when a user clicks a button)
+When it is invoked (called) from JavaScript code
+Automatically (self invoked)
+
+Function Return
+When JavaScript reaches a return statement, the function will stop executing.
+
+If the function was invoked from a statement, JavaScript will "return" to execute the code after the invoking statement.
+
+Functions often compute a return value. The return value is "returned" back to the "caller":
+*/
+
+/*
+Why Functions?
+You can reuse code: Define the code once, and use it many times.
+
+You can use the same code many times with different arguments, to produce different results. */
+
+
