630d764c78b8a98d07992a786f603c1c985f360f
NickBwalley <nickbwalley@gmail.com>
2021-04-13 11:40:20 +0300

    JS Events

---
+++ Events/events.js
@@ -0,0 +1,4 @@
+// JavaScript code is often several lines long. It is more common to see event attributes calling functions:
+function displayTime(){
+    document.getElementById("demo").innerHTML = Date();
+}
