c5115b32efc8ce5febae592b1f606a3d3197da2c
NickBwalley <nickbwalley@gmail.com>
2021-04-13 10:43:13 +0300

    JS Objects

--- Objects/objects.js
+++ Objects/objects.js
@@ -66,3 +66,27 @@ In a function definition, this refers to the "owner" of the function.
 In the example above, this is the person object that "owns" the fullName function.
 
 In other words, this.firstName means the firstName property of this object. */
+
+/*
+Accessing Object Methods
+You access an object method with the following syntax:
+
+objectName.methodName()
+
+Example
+name = person.fullName();
+
+If you access a method without the () parentheses, it will return the function definition:
+
+Example
+name = person.fullName; */
+var myName = person.fullName();
+document.write(myName); // returns John Doe
+
+document.write("<br>");
+
+var MyName2 = person.fullName;
+document.write(MyName2); //  return function definition
+
+document.write("<br>");
+
