e1f941e5f41b30564882e82e89b0083068791f87
NickBwalley <nickbwalley@gmail.com>
2021-04-13 11:42:11 +0300

    JS Events

--- Events/events.js
+++ Events/events.js
@@ -2,3 +2,32 @@
 function displayTime(){
     document.getElementById("demo").innerHTML = Date();
 }
+
+/*
+Common HTML Events
+Here is a list of some common HTML events:
+
+Event	    Description
+onchange	An HTML element has been changed
+onclick	    The user clicks an HTML element
+onmouseover	The user moves the mouse over an HTML element
+onmouseout	The user moves the mouse away from an HTML element
+onkeydown	The user pushes a keyboard key
+onload	    The browser has finished loading the page */
+
+/*
+What can JavaScript Do?
+Event handlers can be used to handle and verify user input, user actions, and browser actions:
+
+Things that should be done every time a page loads
+Things that should be done when the page is closed
+Action that should be performed when a user clicks a button
+Content that should be verified when a user inputs data
+And more ...
+Many different methods can be used to let JavaScript work with events:
+
+HTML event attributes can execute JavaScript code directly
+HTML event attributes can call JavaScript functions
+You can assign your own event handler functions to HTML elements
+You can prevent events from being sent or being handled
+And more ... */
