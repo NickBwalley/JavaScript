36b073c9c5ae3d2449b008e071e60ea84d0f043b
NickBwalley <nickbwalley@gmail.com>
2021-04-20 12:48:43 +0300

    JS Dates

--- Dates/dates.js
+++ Dates/dates.js
@@ -75,3 +75,51 @@ newLine();
 // The toISOString() method converts a date to a string, using the ISO standard format:
 toPrint(a.toISOString());
 newLine();
+
+/*
+JavaScript Date Input
+There are generally 3 types of JavaScript date input formats:
+
+Type	Example
+ISO Date	"2015-03-25" (The International Standard)
+Short Date	"03/25/2015"
+Long Date	"Mar 25 2015" or "25 Mar 2015"
+
+The ISO format follows a strict standard in JavaScript.
+The other formats are not so well defined and might be browser specific. 
+
+
+JavaScript Date Output
+Independent of input format, JavaScript will (by default) output dates in full text string format:
+Wed Mar 25 2015 03:00:00 GMT+0300 (East Africa Time) 
+
+UTC (Universal Time Coordinated) is the same as GMT (Greenwich Mean Time).
+Omitting T or Z in a date-time string can give different results in different browsers.
+
+
+Time Zones
+When setting a date, without specifying the time zone, JavaScript will use the browser's time zone.
+When getting a date, without specifying the time zone, the result is converted to the browser's time zone.
+In other words: If a date/time is created in GMT (Greenwich Mean Time), the date/time will be converted to CDT (Central US Daylight Time) if a user browses from central US.
+JavaScript Short Dates.
+
+Short dates are written with an "MM/DD/YYYY" syntax like this:
+
+JavaScript Long Dates.
+Long dates are most often written with a "MMM DD YYYY" syntax like this:
+
+
+Date Input - Parsing Dates
+If you have a valid date string, you can use the Date.parse() method to convert it to milliseconds.
+Date.parse() returns the number of milliseconds between the date and January 1, 1970:*/
+
+var msec = Date.parse("Mar 21, 2012");
+toPrint(msec);
+newLine();
+// You can then use the number of milliseconds to convert it to a date object:
+var d = new Date(msec);
+toPrint(d);
+newLine();
+
+
+
